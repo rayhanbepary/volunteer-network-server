378558bb654a92cdf1b017a9cec12c67e5d97fa9
rayhanbepary <ceorayhan2000@gmail.com>
2020-10-06 00:03:36 +0600

    change service

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = 5000
 app.use(bodyParser.json());
 app.use(cors());
 
-const serviceAccount = require("./config/volunteer-network-5055d-firebase-adminsdk-qktkt-585731cfab.json");
+const serviceAccount = require(process.env.SER_ACC);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.FIRE_DB
